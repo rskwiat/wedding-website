8b51d008e97b2e4bd7f7c262662d88de460c2dbb
Robert Skwiat <robert@mediabrix.com>
2016-09-12 13:16:22 -0400

    making a seperate webpack production focused file, removing public/index.html

---
+++ webpack.production.js
@@ -0,0 +1,46 @@
+var path = require("path");
+var webpack = require('webpack');
+
+var entry = './app/main.js';
+var path =  __dirname;
+var publicPath = '/';
+var filename = './public/dist/bundle.js';
+
+
+module.exports = {
+  entry: [
+    entry
+  ],
+  output: {
+    path: path,
+    publicPath: publicPath,
+    filename: filename
+  },
+  module: {
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './public'
+  }
+};
