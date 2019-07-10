ce138acdd7a0ae231e15f8611bc7b631a6613246
Robert Skwiat <rskwiat@gmail.com>
2019-07-10 07:49:44 -0400

    deleting old application files

--- src/js/components/__tests__/navigationLink.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import NavigationLink from '../NavigationLink';
-
-test('Application should mount', () => {
-  const component = shallow(<NavigationLink title="test item" url="/" />);
-  expect(component).toMatchSnapshot();
-})
